4a00e00684afc042888e56f81703f88ba7a04084
arif-yusuf-yilmaz3 <y.37ylmaz@gmail.com>
2023-07-13 16:01:45 +0300

    backend initial commit

---
+++ portfolioTrackingAppBackend/src/main/java/com/arifyusufyilmaz/portfolioTrackingApp/PortfolioTrackingAppApplication.java
@@ -0,0 +1,13 @@
+package com.arifyusufyilmaz.portfolioTrackingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioTrackingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioTrackingAppApplication.class, args);
+	}
+
+}
---
+++ portfolioTrackingAppBackend/src/test/java/com/arifyusufyilmaz/portfolioTrackingApp/PortfolioTrackingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arifyusufyilmaz.portfolioTrackingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioTrackingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
