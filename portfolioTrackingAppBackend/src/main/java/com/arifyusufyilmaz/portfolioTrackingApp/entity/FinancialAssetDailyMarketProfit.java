67535e52582627591e8bfa76604611c6be4fb034
arif-yusuf-yilmaz3 <y.37ylmaz@gmail.com>
2023-07-22 23:12:43 +0300

    Added current price field to FinancialAssetDailyMarketProfit

--- portfolioTrackingAppBackend/src/main/java/com/arifyusufyilmaz/portfolioTrackingApp/entity/FinancialAssetDailyMarketProfit.java
+++ portfolioTrackingAppBackend/src/main/java/com/arifyusufyilmaz/portfolioTrackingApp/entity/FinancialAssetDailyMarketProfit.java
@@ -3,12 +3,25 @@ package com.arifyusufyilmaz.portfolioTrackingApp.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+
 @Entity
 @Table(name = "financial_asset_daily_market_profits")
 public class FinancialAssetDailyMarketProfit extends DailyMarketProfit{
 
+    private BigDecimal currentPrice;
+
     @ManyToOne
     private FinancialAsset financialAsset;
+
+    public BigDecimal getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public void setCurrentPrice(BigDecimal currentPrice) {
+        this.currentPrice = currentPrice;
+    }
 
     public FinancialAsset getFinancialAsset() {
         return financialAsset;
